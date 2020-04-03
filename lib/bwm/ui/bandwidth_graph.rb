1f4e17538f9fbf88562c05988c048f1cf415f0ef
mattladany <matt@va-a.com>
2020-04-03 20:22:42 +0000

    Added ascii_chart gem to draw line graphs

--- lib/bwm/ui/bandwidth_graph.rb
+++ lib/bwm/ui/bandwidth_graph.rb
@@ -3,6 +3,7 @@
 require_relative '../bandwidth/bandwidth'
 require 'curses'
 require 'singleton'
+require 'ascii_chart'
 
 module BandwidthMonitor
   module UI
@@ -15,15 +16,20 @@ module BandwidthMonitor
         @bandwidth = Bandwidth.new
 
         @window = Curses::Window.new(Curses.lines / 2 - 1, Curses.cols - 2, 0, 0)
-        @window.box('|', '-')
-        @window.setpos(2, 2)
-        @window.addstr('hello')
+#        @window.box('|', '-')
+        @window.setpos(5, 0)
+#        @window.addstr('hello')
+        @window.addstr(AsciiChart.plot((0...40).map { |i|
+          Math.cos(i * ((Math::PI * 8) / 90)).round(2) * 10
+        }))
+
+        @window.setpos(10,2)
+        @window.addstr("testing1234!")
+
         @window.refresh
       end
 
       def start
-
-        # Loop forever until killed
         loop do
         end
       end
