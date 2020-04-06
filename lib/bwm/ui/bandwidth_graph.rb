55b7972237536e2c9b63c24e74054dd15248bc5f
mattladany <matt@va-a.com>
2020-04-06 18:22:22 +0000

    Added x axis to get ready for adding units

--- lib/bwm/ui/bandwidth_graph.rb
+++ lib/bwm/ui/bandwidth_graph.rb
@@ -10,23 +10,34 @@ module BandwidthMonitor
     class BandwidthGraph
       include Singleton
 
-      attr_reader :window, :bandwidth
+      attr_reader :graph_window, :title_window, :bandwidth
 
       def initialize
         @bandwidth = Bandwidth.new
 
-        @window = Curses::Window.new(Curses.lines / 2 - 1, Curses.cols - 2, 0, 0)
+        @graph_window = Curses::Window.new(Curses.lines / 2 - 1, Curses.cols - 2, 0, 0)
 #        @window.box('|', '-')
-        @window.setpos(5, 0)
+        @graph_window.setpos(5, 0)
 #        @window.addstr('hello')
-        @window.addstr(AsciiChart.plot((0...40).map { |i|
+        @graph_window.addstr(AsciiChart.plot((0...40).map { |i|
           Math.cos(i * ((Math::PI * 8) / 90)).round(2) * 10
-        }))
+        }, {height: (Curses.cols / 4)}))
 
-        @window.setpos(10,2)
-        @window.addstr("testing1234!")
+        @graph_window.refresh
 
-        @window.refresh
+
+        @title_window = Curses::Window.new(1, Curses.cols - 2,
+                                           Curses.lines / 2, 0)
+#        @title_window.box('|','-')
+        @title_window.setpos(0, 3)
+        @title_window.addstr('Mb/s')
+
+        @title_window.setpos(0, Curses.cols / 2 - 1)
+#        @window.addstr('hello')
+        @title_window.addstr("testing1234!")
+
+        @title_window.refresh
+
       end
 
       def start
