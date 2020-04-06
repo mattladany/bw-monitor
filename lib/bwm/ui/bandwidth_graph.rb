083a4bdc33e6e0656225cdb848d95d4032cb9e74
mattladany <matt@va-a.com>
2020-04-06 20:33:30 +0000

    Segemented graph into two windows: x/y plane and x-axis

--- lib/bwm/ui/bandwidth_graph.rb
+++ lib/bwm/ui/bandwidth_graph.rb
@@ -10,39 +10,42 @@ module BandwidthMonitor
     class BandwidthGraph
       include Singleton
 
-      attr_reader :graph_window, :title_window, :bandwidth
+      attr_reader :graph_window, :x_axis_window, :bandwidth
 
       def initialize
         @bandwidth = Bandwidth.new
 
         @graph_window = Curses::Window.new(Curses.lines / 2 - 1, Curses.cols - 2, 0, 0)
-#        @window.box('|', '-')
-        @graph_window.setpos(5, 0)
-#        @window.addstr('hello')
-        @graph_window.addstr(AsciiChart.plot((0...40).map { |i|
+
+        @x_axis_window = Curses::Window.new(1, Curses.cols - 2,
+                                           Curses.lines / 2, 0)
+#        @title_window.box('|','-')
+      end
+
+      def start
+        initialize_graph
+        initialize_x_axis
+        loop do
+        end
+      end
+
+      def initialize_graph
+        @graph_window.setpos(3, 0)
+        @graph_window.addstr(AsciiChart.plot((0...89).map { |i|
           Math.cos(i * ((Math::PI * 8) / 90)).round(2) * 10
         }, {height: (Curses.cols / 4)}))
 
         @graph_window.refresh
-
-
-        @title_window = Curses::Window.new(1, Curses.cols - 2,
-                                           Curses.lines / 2, 0)
-#        @title_window.box('|','-')
-        @title_window.setpos(0, 3)
-        @title_window.addstr('Mb/s')
-
-        @title_window.setpos(0, Curses.cols / 2 - 1)
-#        @window.addstr('hello')
-        @title_window.addstr("testing1234!")
-
-        @title_window.refresh
-
       end
 
-      def start
-        loop do
-        end
+      def initialize_x_axis
+        @x_axis_window.setpos(0, 3)
+        @x_axis_window.addstr('Mb/s')
+
+        @x_axis_window.setpos(0, Curses.cols / 2 - 1)
+        @x_axis_window.addstr("testing1234!")
+
+        @x_axis_window.refresh
       end
     end
   end
