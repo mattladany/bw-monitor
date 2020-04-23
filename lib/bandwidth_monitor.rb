0382505ea104a8a20ea8a00606a618559e144ac2
mattladany <matt@va-a.com>
2020-04-22 23:08:53 -0400

    Created CLI class for handling cli options
    Also cleaned up some rubocop recommendations

--- lib/bandwidth_monitor.rb
+++ lib/bandwidth_monitor.rb
@@ -1,29 +1,10 @@
 # frozen_string_literal: true
 
-require_relative 'bwm/version'
-require_relative 'bwm/ui/ui'
 require 'curses'
 
-# The core module for the throughput-monitor project.
-module BandwidthMonitor
-  class Error < StandardError; end
-
-  def init_curses
-    # start ncurses
-    Curses.noecho
-    Curses.init_screen
-    Curses.start_color
-    Curses.stdscr.keypad = true
-    Curses.curs_set(0)
-  end
-
-  def run
-    init_curses
-    ui = UI::UIController.new
-    ui.start
-  ensure
-    Curses.close_screen
-  end
-
-  module_function :run, :init_curses
-end
+require_relative 'bwm/version'
+require_relative 'bwm/cli'
+require_relative 'bwm/bandwidth/bandwidth.rb'
+require_relative 'bwm/ui/ui'
+require_relative 'bwm/ui/menu'
+require_relative 'bwm/ui/bandwidth_graph'
---
+++ lib/bwm/cli.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require_relative 'ui/ui'
+
+require 'curses'
+
+module BandwidthMonitor
+  class Error < StandardError; end
+  # The CLI class is in charge of all command line interface processing as
+  # well as starting the curses app if all logic passes. Note: This is closely
+  # modeled after the rubocop gem's CLI class, at
+  # https://github.com/rubocop-hq/rubocop/blob/master/lib/rubocop/cli.rb.
+  class CLI
+    STATUS_SUCCESS = 0
+    STATUS_FAILURE = 1
+    def initialize; end
+
+    # Initialize the curses screen.
+    def init_curses
+      # start ncurses
+      Curses.noecho
+      Curses.init_screen
+      Curses.start_color
+      Curses.stdscr.keypad = true
+    end
+
+    # The entry point to the application. First parse the command line
+    # arguments and then run the curses application until it is killed or quit.
+    #
+    # @param args [Array<String>] command line arguments
+    # @return [Integer] the status code after processing the execution
+    #   parameters.
+    def run(args = ARGV)
+      ready_to_run = process_arguments(args)
+      if ready_to_run
+        run_app
+        return STATUS_SUCCESS
+      end
+      STATUS_FAILURE
+    end
+
+    def process_arguments(_args); end
+
+    def run_app
+      init_curses
+      ui = UI::UIController.new
+      ui.start
+    ensure
+      Curses.close_screen
+    end
+  end
+end
